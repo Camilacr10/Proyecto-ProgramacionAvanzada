d0e2efc063a3648292249e6812fbfc3b7ad856c8
Dramirez00467 <106043587+Dramirez00467@users.noreply.github.com>
2025-03-03 21:13:06 -0600

    Se agregaron los controller de Adopciones, donaciones, usuarios

---
+++ ProyectoPA/Controllers/AdopcionsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoPA;
+
+namespace ProyectoPA.Controllers
+{
+    public class AdopcionsController : Controller
+    {
+        private salvando_unas_patitasEntities db = new salvando_unas_patitasEntities();
+
+        // GET: Adopcions
+        public ActionResult Index()
+        {
+            var adopcions = db.Adopcions.Include(a => a.Mascota).Include(a => a.Usuario);
+            return View(adopcions.ToList());
+        }
+
+        // GET: Adopcions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adopcion adopcion = db.Adopcions.Find(id);
+            if (adopcion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adopcion);
+        }
+
+        // GET: Adopcions/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_mascota = new SelectList(db.Mascotas, "id_mascota", "nombre");
+            ViewBag.id_usuario = new SelectList(db.Usuarios, "id_usuario", "username");
+            return View();
+        }
+
+        // POST: Adopcions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_adopcion,id_usuario,id_mascota,fecha_adopcion")] Adopcion adopcion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Adopcions.Add(adopcion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_mascota = new SelectList(db.Mascotas, "id_mascota", "nombre", adopcion.id_mascota);
+            ViewBag.id_usuario = new SelectList(db.Usuarios, "id_usuario", "username", adopcion.id_usuario);
+            return View(adopcion);
+        }
+
+        // GET: Adopcions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adopcion adopcion = db.Adopcions.Find(id);
+            if (adopcion == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_mascota = new SelectList(db.Mascotas, "id_mascota", "nombre", adopcion.id_mascota);
+            ViewBag.id_usuario = new SelectList(db.Usuarios, "id_usuario", "username", adopcion.id_usuario);
+            return View(adopcion);
+        }
+
+        // POST: Adopcions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_adopcion,id_usuario,id_mascota,fecha_adopcion")] Adopcion adopcion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(adopcion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_mascota = new SelectList(db.Mascotas, "id_mascota", "nombre", adopcion.id_mascota);
+            ViewBag.id_usuario = new SelectList(db.Usuarios, "id_usuario", "username", adopcion.id_usuario);
+            return View(adopcion);
+        }
+
+        // GET: Adopcions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adopcion adopcion = db.Adopcions.Find(id);
+            if (adopcion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adopcion);
+        }
+
+        // POST: Adopcions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Adopcion adopcion = db.Adopcions.Find(id);
+            db.Adopcions.Remove(adopcion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoPA/Controllers/DonacionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoPA;
+
+namespace ProyectoPA.Controllers
+{
+    public class DonacionsController : Controller
+    {
+        private salvando_unas_patitasEntities db = new salvando_unas_patitasEntities();
+
+        // GET: Donacions
+        public ActionResult Index()
+        {
+            return View(db.Donacions.ToList());
+        }
+
+        // GET: Donacions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donacion donacion = db.Donacions.Find(id);
+            if (donacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donacion);
+        }
+
+        // GET: Donacions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Donacions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_donacion,monto,nombre_donante,email_donante,metodo_pago,numero_tarjeta,fecha_expiracion,codigo_seguridad,fecha_donacion")] Donacion donacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donacions.Add(donacion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(donacion);
+        }
+
+        // GET: Donacions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donacion donacion = db.Donacions.Find(id);
+            if (donacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donacion);
+        }
+
+        // POST: Donacions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_donacion,monto,nombre_donante,email_donante,metodo_pago,numero_tarjeta,fecha_expiracion,codigo_seguridad,fecha_donacion")] Donacion donacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donacion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(donacion);
+        }
+
+        // GET: Donacions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donacion donacion = db.Donacions.Find(id);
+            if (donacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donacion);
+        }
+
+        // POST: Donacions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Donacion donacion = db.Donacions.Find(id);
+            db.Donacions.Remove(donacion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
