184a108cacb19e84ebb1ecd9f088214d6921da74
Camilacr10 <130608400+Camilacr10@users.noreply.github.com>
2025-03-10 19:37:41 -0600

    Creacion de la interfaz Listado de Mascotas (Falta logica con la Base de Datos)

---
+++ ProyectoPA/Controllers/ListadoMController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoPA.Controllers
+{
+   
+        public class ListadoMController : Controller
+        {
+            private salvando_unas_patitasEntities db = new salvando_unas_patitasEntities();
+
+            // Acción para el listado de mascotas
+            public ActionResult listado()
+            {
+                return View();
+            }
+        }
+    }
+
