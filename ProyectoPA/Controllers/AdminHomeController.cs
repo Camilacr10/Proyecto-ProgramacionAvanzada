b6e9b4445d1663f1b4eecdeb9c9a12edd4202aeb
Dramirez00467 <106043587+Dramirez00467@users.noreply.github.com>
2025-03-12 09:25:21 -0600

    Cambios en la parte de admin
    Se quiso dejar solo un layout y un menú.

---
+++ ProyectoPA/Controllers/AdminHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoPA.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        //Se visualiza el menu de administrador
+        public ActionResult MenuAdmin()
+        {
+            return View();
+        }
+    }
+}
