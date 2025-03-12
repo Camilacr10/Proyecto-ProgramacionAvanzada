8abad4865812e5bec2ac8bc94a703a7efb89dc2b
Camilacr10 <130608400+Camilacr10@users.noreply.github.com>
2025-03-11 19:12:43 -0600

    Interfaz de listado de Mascotas y Perfil de Mascotas totalmente funcional

--- ProyectoPA/Controllers/ListadoMController.cs
+++ ProyectoPA/Controllers/ListadoMController.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace ProyectoPA.Controllers
 {
-   
-        public class ListadoMController : Controller
-        {
-            private salvando_unas_patitasEntities db = new salvando_unas_patitasEntities();
 
-            // Acción para el listado de mascotas
-            public ActionResult listado()
-            {
-                return View();
-            }
+    public class ListadoMController : Controller
+    {
+        private salvando_unas_patitasEntities db = new salvando_unas_patitasEntities();
+
+        // Acción para el listado de mascotas
+        public ActionResult listado()
+        {
+            var listadoMascotas = db.Mascotas.Where(m => m.disponibilidad == "Si").ToList();
+            return View(listadoMascotas);
         }
+
+        public ActionResult Detalles(int id)
+        {
+            var mascota = db.Mascotas.FirstOrDefault(m => m.id_mascota == id);
+
+            if (mascota == null)
+            {
+                return HttpNotFound(); // Retorna error 404 si no encuentra la mascota
+            }
+
+            return View(mascota); // Retorna la vista detalles.cshtml con la información de la mascota
+        }
+
+
     }
+}
 
